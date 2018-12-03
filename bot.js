927471898893a7c06ca2ceb023cc1330c5ca83c3
PrinceSkyLeGeND <43113649+PrinceSkyLeGeND@users.noreply.github.com>
2018-12-02 19:02:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
+client.login(process.env.NTE4ODIwOTYzNjQ3ODgxMjE3.DuYCEw.zxn2qyNDYx5ZW8AFy8JdE6ZOuFk);
