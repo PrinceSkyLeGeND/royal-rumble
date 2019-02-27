c8634f886e7aecbef62f0c5c10d2a887c3608fa8
PrinceSkyLeGeND <43113649+PrinceSkyLeGeND@users.noreply.github.com>
2019-02-26 21:27:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e)); 
+client.on("debug", (e) => console.info(e));
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
