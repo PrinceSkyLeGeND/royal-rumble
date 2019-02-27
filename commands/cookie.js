d1b70a098b71e6f521ff2efdee806dd70a50a162
PrinceSkyLeGeND <43113649+PrinceSkyLeGeND@users.noreply.github.com>
2019-02-26 21:25:06 -0500

    Create cookie.js

---
+++ commands/cookie.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, [mention, ...reason]) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g); const command = args.shift().toLowerCase(); 
+    
+if(command === 'cookie') {
+
+    let user = message.mentions.users.first();
+    let reason = args.slice(1).join(" ") ||  "Nothing";
+
+    if(!user) {
+        return message.channel.send('Select a user.');
+    }
+    
+    console.log(reason)
+
+    message.channel.send('**'+ user.username +',** you got a :cookie: from **'+message.author.username+'**\n\n**Because:** '+reason+'\n(づ｡◕‿‿◕｡)づ:･ﾟ✧ :cookie:'); 
+ }
+}
